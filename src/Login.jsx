2b563f6a976e5e096faa6b541de4f28831243a25
jeeva m <jeelion22@gmail.com>
2024-04-30 12:18:20 +0530

    added addtional links for those pages

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,73 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div class="row justify-content-center text-center mt-4">
+        <div class="col-md-6">
+          <h4>Log In</h4>
+        </div>
+      </div>
+
+      <div class="row justify-content-center mt-3">
+        <div class="col-md-6">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Email"
+            aria-label="First name"
+          />
+        </div>
+      </div>
+
+      <div class="row justify-content-center mt-3">
+        <div class="col-md-6">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Password"
+            aria-label="First name"
+          />
+        </div>
+      </div>
+
+      <div class="row justify-content-center mt-3">
+        <div class="col-md-3 d-flex">
+          <div className="existing-account">
+            <span>
+              <Link to={"/forgotpassword"}>Forgot Password?</Link>
+            </span>
+          </div>
+        </div>
+        <div class="col-md-3 justify-content-end">
+          <div className=" d-flex justify-content-end">
+            <button type="submit" className="btn btn-primary">
+              Log in
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center mt-5">
+        <div class="col-md-6 text-center">
+          <span>or</span>
+        </div>
+      </div>
+
+      <div class="row justify-content-center mt-5">
+        <div class="col-md-3 d-flex">
+          <div className="create-account">
+            <span>For create an account </span>
+            <span>
+              <Link to={"/signup"}>Click Here</Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -4,73 +4,79 @@ import { Link } from "react-router-dom";
 const Signup = () => {
   return (
     <>
-      <div className="container-md-6 border p-5 m-5 rounded">
-        <div class="row justify-content-center text-center mt-4">
-          <div class="col-md-6">
-            <h4>Sign Up</h4>
-          </div>
-        </div>
-
-        <div class="row justify-content-center mt-4">
-          <div class="col-md-3">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="First name"
-              aria-label="First name"
-            />
-          </div>
-          <div class="col-md-3">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Last name"
-              aria-label="Last name"
-            />
-          </div>
-        </div>
-        <div class="row justify-content-center mt-3">
-          <div class="col-md-6">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Email"
-              aria-label="First name"
-            />
-          </div>
-        </div>
-        <div class="row justify-content-center mt-3">
-          <div class="col-md-6">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Password"
-              aria-label="First name"
-            />
-          </div>
-        </div>
-        <div class="row justify-content-center mt-3">
-          <div class="col-md-6">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Confirm Password"
-              aria-label="First name"
-            />
-          </div>
-        </div>
-        <div class="row justify-content-center mt-3">
-          <div class="col-md-3 d-flex">
-            <div className="existing-account">Already have an account?</div>
-            <div className="login ms-2">
-              <Link to={"/login"}>Log In</Link>
+      <div className="container-md-6  p-5">
+        <div className="row">
+          <div className="col">
+            <div class="row justify-content-center text-center mt-4">
+              <div class="col-md-6">
+                <h4>Sign Up</h4>
+              </div>
             </div>
-          </div>
-          <div class="col-md-3 justify-content-end">
-            <div className=" d-flex justify-content-end">
-              <button type="submit" className="btn btn-primary">
-                Create Account
-              </button>
+
+            <div class="row justify-content-center mt-4">
+              <div class="col-md-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="First name"
+                  aria-label="First name"
+                />
+              </div>
+              <div class="col-md-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Last name"
+                  aria-label="Last name"
+                />
+              </div>
+            </div>
+            <div class="row justify-content-center mt-3">
+              <div class="col-md-6">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Email"
+                  aria-label="First name"
+                />
+              </div>
+            </div>
+            <div class="row justify-content-center mt-3">
+              <div class="col-md-6">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Password"
+                  aria-label="First name"
+                />
+              </div>
+            </div>
+            <div class="row justify-content-center mt-3">
+              <div class="col-md-6">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Confirm Password"
+                  aria-label="First name"
+                />
+              </div>
+            </div>
+            <div class="row justify-content-center mt-3">
+              <div class="col-md-3 d-flex">
+                <div className="existing-account">
+                  <span>Already have an account? </span>
+                  <span>
+                    <Link to={"/login"}>Log In</Link>
+                  </span>
+                </div>
+              </div>
+              <div class="col-md-3 justify-content-end">
+                <div className=" d-flex justify-content-end">
+                  <button type="submit" className="btn btn-primary">
+                    Create Account
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>

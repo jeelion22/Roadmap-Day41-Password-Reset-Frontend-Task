5aad340b559948c1a3464918d480ac8a7dc5d634
jeeva m <jeelion22@gmail.com>
2024-04-30 12:17:39 +0530

    added forgetpassword and reset password routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Signup from "./Signup";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Login from "./Login";
+import Forgotpassword from "./Forgotpassword";
+import Resetpassword from "./Resetpassword";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgotPassword" element={<Forgotpassword />} />
+        <Route path="/resetPassword/:resetToken" element={<Resetpassword />} />
       </Routes>
     </>
   );
